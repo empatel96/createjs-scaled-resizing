7154139e5262a148801533ef81c69fa5e7cecd8c
Muhammed Patel <42419839+empatel96@users.noreply.github.com>
2020-02-19 16:17:55 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var ratio, windowRatio;
 var w = window.innerWidth,
   h = window.innerHeight;
-  var scale = w/1024;
+  var scale = w/1024; // w/width
 function handleResize(event) {
-  ratio = 1024/768;
+  ratio = 1024/768; // width/height
   windowRatio = w/h;
   // Layout other assets
   if (windowRatio > ratio) {
-    scale = h/768;
+    scale = h/768; // h/height
   }
   stage.update();
 }
